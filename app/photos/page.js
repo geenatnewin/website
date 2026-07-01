2aca7f99304c8ce4f93ee9fd432916f9f31760e9
Navin Nguyen <geenatnewin@gmail.com>
2026-06-30 19:53:42 -0700

    Move Life positions 5/6/7 to 11/12/13

--- app/photos/page.js
+++ app/photos/page.js
@@ -5,9 +5,9 @@ import Link from 'next/link'
 
 const lifePhotos = [
   '4I0A1194.jpg','4I0A1276.jpg','4I0A1282.jpg',
-  '4I0A1302.jpg','4I0A1780.jpg','4I0A1810.jpg',
-  'IMG_0177.jpg','4I0A2746.jpg','4I0A2752.jpg','4I0A2762.jpg',
+  '4I0A1302.jpg','4I0A2746.jpg','4I0A2752.jpg','4I0A2762.jpg',
   '4I0A2920.jpg','DSC05265.jpg','DSC05382.jpg',
+  '4I0A1780.jpg','4I0A1810.jpg','IMG_0177.jpg',
   '4I0A1409.jpg','4I0A1414.jpg','DSC05425.jpg',
   'DSC05448.jpg','DSC05506.jpg','DSC05766.jpg','DSC05777.jpg',
   'IMG_0007.jpg','IMG_0095.jpg','IMG_0099.jpg','IMG_0840.jpg',
